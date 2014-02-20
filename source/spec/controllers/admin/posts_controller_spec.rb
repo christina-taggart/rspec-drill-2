5b9081562a5e309fadcde23bb553289ebbaad7da
Apprentice <apprentice@dbc22.local>
2014-02-19 18:21:43 -0800

    Just committing to move to another workstation

--- source/spec/controllers/admin/posts_controller_spec.rb
+++ source/spec/controllers/admin/posts_controller_spec.rb
@@ -25,17 +25,26 @@ describe Admin::PostsController do
       end
     end
 
-    context "#edit" do
+    context "#update" do
+      let(:post) { Post.create(title: "This is a title", content: "This is some content.")}
       it "updates a post with valid params" do
-        pending
+        expect {
+          post.update_attributes(title: "This is an awesome title")
+        }.to change{post.title}
       end
       it "doesn't update a post when params are invalid" do
         pending
+        expect {
+          post.update_attributes(title: nil)
+        }.not_to change{post.title}
       end
     end
 
+    let(:post) { Post.create(title: "Blob", content: "Blob") }
     it "#destroy" do
-      pending
+      expect {
+        delete :destroy, id: post.id
+      }.to change(Post, :count)
     end
   end
 end
