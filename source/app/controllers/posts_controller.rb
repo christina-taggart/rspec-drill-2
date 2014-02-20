7ff0a754ffbf8fe6f623cfc7f83a9a7d2aff5a5c
Apprentice <apprentice@dbc22.local>
2014-02-19 16:23:38 -0800

    Adds implementation for feature, 'Admin panel on admin homepage can create a new post and view it'

--- source/app/controllers/posts_controller.rb
+++ source/app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      flash[:notice] = "Published: true.  Post was successfully saved."
+      redirect_to @post
+    else
+      render :new
+    end
+  end
 end
