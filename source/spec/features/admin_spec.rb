5438e4dc2080eba3fb701de9a35c612356d9d002
John Olmsted <apprentice@dbc21.local>
2014-02-19 17:14:58 -0800

    Adds pending examples to features/admin_spec.rb, 2 still pending

--- source/spec/features/admin_spec.rb
+++ source/spec/features/admin_spec.rb
@@ -2,11 +2,28 @@ require 'spec_helper'
 
 feature 'Admin panel' do
   context "on admin homepage" do
-    it "can see a list of recent posts"
+    let!(:post) { FactoryGirl.create :post }
+    it "can see a list of recent posts" do
+      visit admin_posts_path
+      expect(page).to have_content(post.title)
+    end
 
-    it "can edit a post by clicking the edit link next to a post"
+    it "can edit a post by clicking the edit link next to a post" do
+      visit admin_posts_path
+      expect{
+        click_link "Edit"
+        fill_in 'post_title', with: "terminator"
+        page.check('post_is_published')
+        click_button "Save"
+      }.to change { post.reload.title }.to("terminator")
+    end
 
-    it "can delete a post by clicking the delete link next to a post"
+    it "can delete a post by clicking the delete link next to a post" do
+      visit admin_posts_path
+      expect{
+        click_link "Delete"
+      }.to change{ Post.count}.by(-1)
+    end
 
     it "can create a new post and view it" do
        visit new_admin_post_url
@@ -24,15 +41,23 @@ feature 'Admin panel' do
   end
 
   context "editing post" do
+    let!(:post) { FactoryGirl.create :post }
     it "can mark an existing post as unpublished" do
-      pending # remove this line when you're working on implementing this test
-
+      visit admin_posts_path
+      click_link "Edit"
+      fill_in 'post_title', with: "terminator"
+      click_button "Save"
       page.should have_content "Published: false"
     end
   end
 
   context "on post show page" do
-    it "can visit a post show page by clicking the title"
+    let!(:post) { FactoryGirl.create :post }
+    it "can visit a post show page by clicking the title" do
+      visit admin_posts_path
+      click_link post.title
+      expect(page).to have_content(post.content)
+    end
 
     it "can see an edit link that takes you to the edit post path"
 
